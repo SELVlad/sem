28e37a3574c672bf7e2be5cbf1ce8c9202ad076b
goususlugi <94773273+goususlugi@users.noreply.github.com>
2022-05-13 23:12:41 +0300

    Семестровая работа по АИСД #1

---
+++ Semestrovka/CreateFile.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.File; 
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
+
+public class CreateFile {
+
+	public static void createFile(File file) throws IOException {
+		try {
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e);
+		}
+
+	}
+	
+	public static void addNumber(File file, int size) throws IOException {
+
+		try {
+
+			int[] numbers = new int[size];
+			
+			Random rnd = new Random();
+			FileWriter fw = new FileWriter(file);
+
+			for (int i = 0; i < numbers.length; i++) {
+				numbers[i] = rnd.nextInt(10000);
+				fw.write(numbers[i] + " ");
+			}
+			
+			fw.close();
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e);	
+		}
+
+	}
+
+}
+
+
